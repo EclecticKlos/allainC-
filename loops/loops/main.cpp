05886e93416c8dd22ebc45105d851e595a4ce50c
Dan Klos <dsklos@gmail.com>
2016-10-25 19:29:24 -0700

    Chap 5

---
+++ loops/loops/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  loops
+//
+//  Created by Dan Klos on 10/25/16.
+//  Copyright © 2016 Dan Klos. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Problem 1
+//    int count = 99;
+//    while (count > 0){
+//        cout << count << " bottles of beer on the wall, " << count << " bottles of beer" << "\n" << "<drank> "  << "\n" << count -1 << " bottles of beer on the wall" << "\n";
+//        count--;
+//    }
+    
+    
+}
